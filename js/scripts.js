b26e8bdd84981e9fd1328ac0ad361272ea65e1d9
sailesh-mahat <50069461+sailesh-mahat@users.noreply.github.com>
2019-05-19 13:03:53 +0200

    ForEach funtion for displaying Pokemon
    forEarch instead of for loop used for displaying Pokemon details.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,22 @@
+//defining an array of pokemon objects
 var repository = [
-  {name:'Bulbasaur', height:0.7, types: ['grass', 'poison']},
-  {name:'Squirtle', height:0.5, types: ['water']},
-  {name:'Butterfree', height:1.1, types: ['bug','flying']},
-  {name:'Dragonite', height:2.2, types: ['dragon','flying']},
-  {name:'Heatmor', height:1.4, types: ['fire']}
+    {name:'Bulbasaur', height:0.7, types: ['grass', 'poison']},
+    {name:'Squirtle', height:0.5, types: ['water']},
+    {name:'Butterfree', height:1.1, types: ['bug','flying']},
+    {name:'Dragonite', height:2.2, types: ['dragon','flying']},
+    {name:'Heatmor', height:1.4, types: ['fire']}
 ];
 
-for (var i = 0; i < repository.length; i++){
-  document.write("<br>" + repository[i].name + "  ");
-  document.write("(height:" + repository[i].height + ")<br>");
-  if (repository[i].height >= 2.0){
-    document.write('Wow! That\'s big.<br>');
+//loop for displaying the pokemons
+//for (var i = 0; i < repository.length; i++){
+//  document.write("<br><br>" + repository[i].name + "  ");
+//  document.write("(height:" + repository[i].height + ")<br>");
+
+//replacing for loop with foreach function
+repository.forEach(function(currentPokemon) {
+  document.write("<p>" + currentPokemon.name);
+  document.write('(Height:' + currentPokemon.height +")");
+  if (currentPokemon.height >= 2.0){
+    document.write('Wow! That\'s big.</p>');
   }
-}
+});
