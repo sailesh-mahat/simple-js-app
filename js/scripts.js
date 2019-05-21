11e8d6cb96b7728d2c0c61bc7265b0459551653f
sailesh-mahat <50069461+sailesh-mahat@users.noreply.github.com>
2019-05-21 23:56:33 +0200

    Task 1.5 (successful version)
    code wrapped in IIFE, functions add() and getAll() used to access the repository array,

--- js/scripts.js
+++ js/scripts.js
@@ -33,14 +33,20 @@ var pokemonRepository =  (function () {
 //repository.forEach(function(currentPokemon) {
   //document.write("<p>" + currentPokemon.name +" ");
   //document.write('(Height:' + currentPokemon.height +")");
-document.write(pokemonRepository.getAll());
+
+//using add() to push objects to the repository array
 pokemonRepository.add({name:'Bulbasaur', height:0.7, types: ['grass', 'poison']});
 pokemonRepository.add({name:'Squirtle', height:0.5, types: ['water']}) + "</p>";
 pokemonRepository.add({name:'Butterfree', height:1.1, types: ['bug','flying']});
 pokemonRepository.add({name:'Dragonite', height:2.2, types: ['dragon','flying']});
 pokemonRepository.add({name:'Heatmor', height:1.4, types: ['fire']});
-document.write(pokemonRepository.getAll());
-//  if (currentPokemon.height >= 2.0){
-  //  document.write('Wow! That\'s big.</p>');
-  //}
+//using getAll() to display the added repository items
+pokemonRepository.getAll().forEach(function(currentPokemon) {
+  document.write("<p>" + currentPokemon.name +" ");
+  document.write('(Height:' + currentPokemon.height +")");
+  if (currentPokemon.height >= 2.0){
+    document.write('Wow! That\'s big.</p>');
+  }
+});
+
 //});
