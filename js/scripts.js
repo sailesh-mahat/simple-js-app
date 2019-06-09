799fc05c745b8a53ce1cf603582e4b31c9242787
sailesh-mahat <50069461+sailesh-mahat@users.noreply.github.com>
2019-06-09 23:31:55 +0200

    addListItem and showDetails functions added (task 1.6)

--- js/scripts.js
+++ js/scripts.js
@@ -1,58 +1,95 @@
-//defining an array of pokemon objects
-//wrapping in IIFE
-var pokemonRepository =  (function () {
-  var repository = [
-    //{name:'Bulbasaur', height:0.7, types: ['grass', 'poison']},
-    //{name:'Squirtle', height:0.5, types: ['water']},
-    //{name:'Butterfree', height:1.1, types: ['bug','flying']},
-    //{name:'Dragonite', height:2.2, types: ['dragon','flying']},
-    //{name:'Heatmor', height:1.4, types: ['fire']}
-  ];
 
-  function add(pokemon) {
-    if (typeof(pokemon) === 'object') {
-      repository.push(pokemon);
-    }
-      //else {
-       //document.write('Please ensure your pokemon has the correct format: {name: (string), height: (number), types: [array of strings]}');
-     //}
+//wrapping in IIFE
+var pokemonRepository = (function() {
+//defining an array of pokemon objects
+  var repository = [];
+
+  var bulbasaur = {
+    name : 'Bulbasaur',
+    height : 0.7,
+    type : ['grass', 'poison']
+  };
+  var squirtle = {
+    name : 'Squirtle',
+    height : 0.5,
+    type : ['water']
+  };
+  var charmander = {
+    name : 'Charmander',
+    height : 0.6,
+    type : ['fire']
+  };
+  var butterfree = {
+    name : 'Butterfree',
+    height : 1.1,
+    type : ['bug', 'flying']
+  };
+  var dragonite = {
+    name : 'Dragonite',
+    height : 2.2,
+    type : ['dragon', 'flying']
+  };
+  var krookodile = {
+    name : 'Krookodile',
+    height : 1.5,
+    type : ['dark', 'ground']
+  };
+
+
+  //pushing the Pokemon objects into the repository array
+  repository.push(bulbasaur, squirtle, charmander, butterfree, dragonite, krookodile);
+
+  //showDetails funtion added
+  function showDetails(pokemonItem) {
+    console.log(pokemonItem.name);
   }
 
+  //add-list-item function
+  function addListItem(pokemonItem) {
+    //text on $p
+    var listItemText = document.createTextNode(pokemonItem.name);
+     //text on $infoButton
+    var buttonText = document.createTextNode('view details');
+    //creating DOM elements
+    var $p = document.createElement('p');
+    var $infoButton = document.createElement('button');
+    var $li = document.createElement('li');
+    var $ul = document.querySelector('ul');
+    //adding classes to elements
+    $infoButton.classList.add('info-button');
+    $li.classList.add('list-item');
+    $ul.classList.add('pokemon-list');
+    //appending elements to the DOM
+    $infoButton.appendChild(buttonText);
+    $p.appendChild(listItemText);
+    $li.appendChild($p);
+    $li.appendChild($infoButton);
+    $ul.appendChild($li);
+    //eventListener added on button on click
+    $infoButton.addEventListener('click', function(event) {
+    showDetails(pokemonItem);
+    });
+  }
 
+  //getAll function
   function getAll() {
     return repository;
   }
 
-  return {
-    add : add,
-    getAll : getAll
-  };
-})();
-
-
-//loop for displaying the pokemons
-//for (var i = 0; i < repository.length; i++){
-//  document.write("<br><br>" + repository[i].name + "  ");
-//  document.write("(height:" + repository[i].height + ")<br>");
-
-//replacing for loop with foreach function
-//repository.forEach(function(currentPokemon) {
-  //document.write("<p>" + currentPokemon.name +" ");
-  //document.write('(Height:' + currentPokemon.height +")");
-
-//using add() to push objects to the repository array
-pokemonRepository.add({name:'Bulbasaur', height:0.7, types: ['grass', 'poison']});
-pokemonRepository.add({name:'Squirtle', height:0.5, types: ['water']}) + "</p>";
-pokemonRepository.add({name:'Butterfree', height:1.1, types: ['bug','flying']});
-pokemonRepository.add({name:'Dragonite', height:2.2, types: ['dragon','flying']});
-pokemonRepository.add({name:'Heatmor', height:1.4, types: ['fire']});
-//using getAll() to display the added repository items
-pokemonRepository.getAll().forEach(function(currentPokemon) {
-  //document.write("<p>" + currentPokemon.name +" ");
-  //document.write('(Height:' + currentPokemon.height +")");
-  if (currentPokemon.height >= 2.0){
-    //document.write('Wow! That\'s big.</p>');
+  //add function to add objects into repository
+  function add(pokemon) {
+  repository.push(pokemon);
   }
-});
 
-//});
+  //return function:
+ return {
+   add: add,
+   getAll: getAll,
+   addListItem: addListItem
+  };
+})(); // IIFE wrapping closed
+
+//calling addListItem for each Pokemon
+pokemonRepository.getAll().forEach(function(pokemonItem) {
+  pokemonRepository.addListItem(pokemonItem);
+});
