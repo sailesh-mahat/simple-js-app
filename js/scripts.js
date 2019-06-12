bd196d5b3ce8d1c867e6c4ebae6b798651e1b4e1
sailesh-mahat <50069461+sailesh-mahat@users.noreply.github.com>
2019-06-12 22:39:36 +0200

    basic styling changes to support new code

--- js/scripts.js
+++ js/scripts.js
@@ -3,46 +3,8 @@
 var pokemonRepository = (function() {
 //defining an array of pokemon objects
   var repository = [];
+  var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-  var bulbasaur = {
-    name : 'Bulbasaur',
-    height : 0.7,
-    type : ['grass', 'poison']
-  };
-  var squirtle = {
-    name : 'Squirtle',
-    height : 0.5,
-    type : ['water']
-  };
-  var charmander = {
-    name : 'Charmander',
-    height : 0.6,
-    type : ['fire']
-  };
-  var butterfree = {
-    name : 'Butterfree',
-    height : 1.1,
-    type : ['bug', 'flying']
-  };
-  var dragonite = {
-    name : 'Dragonite',
-    height : 2.2,
-    type : ['dragon', 'flying']
-  };
-  var krookodile = {
-    name : 'Krookodile',
-    height : 1.5,
-    type : ['dark', 'ground']
-  };
-
-
-  //pushing the Pokemon objects into the repository array
-  repository.push(bulbasaur, squirtle, charmander, butterfree, dragonite, krookodile);
-
-  //showDetails funtion added
-  function showDetails(pokemonItem) {
-    console.log(pokemonItem.name);
-  }
 
   //add-list-item function
   function addListItem(pokemonItem) {
@@ -71,25 +33,74 @@ var pokemonRepository = (function() {
     });
   }
 
+  //showDetails funtion added
+  function showDetails(pokemonItem) {
+    pokemonRepository.loadDetails(pokemonItem).then(function() {
+    console.log(pokemonItem); });
+  }
+
+  //add function to add objects into repository
+  function add(pokemon) {
+    repository.push(pokemon);
+  }
+
   //getAll function
   function getAll() {
     return repository;
   }
 
-  //add function to add objects into repository
-  function add(pokemon) {
-  repository.push(pokemon);
+
+  function loadList() {
+    //fetches the data from API
+      return fetch(apiUrl, {
+        method: 'GET'
+      }).then(function (response) {
+        return response.json();
+      }).then(function (json) {
+        json.results.forEach(function (item) {
+          var pokemon = {
+            name: item.name,
+            detailsUrl: item.url
+          };
+          add(pokemon);
+        });
+      }).catch(function (e) {
+        console.error(e);
+      })
+    }
+
+//loads detailed data for each pokemon using detailsUrl property
+    function loadDetails(item) {
+    var url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      // Now we add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = Object.keys(details.types);
+    }).catch(function (e) {
+      console.error(e);
+    });
   }
+
 
   //return function:
  return {
    add: add,
    getAll: getAll,
-   addListItem: addListItem
+   addListItem: addListItem,
+   showDetails: showDetails,
+   //search: search,
+   loadList: loadList,
+   loadDetails: loadDetails
   };
 })(); // IIFE wrapping closed
 
-//calling addListItem for each Pokemon
-pokemonRepository.getAll().forEach(function(pokemonItem) {
-  pokemonRepository.addListItem(pokemonItem);
+// Loading the data from API
+pokemonRepository.loadList().then(function() {
+  // Now the data is loaded!
+  pokemonRepository.getAll().forEach(function(pokemonItem){
+    pokemonRepository.addListItem(pokemonItem);
+  });
 });
