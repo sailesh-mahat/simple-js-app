de869360333c62099c46fa446f25567afb7b84c5
sailesh-mahat <50069461+sailesh-mahat@users.noreply.github.com>
2019-06-13 22:43:20 +0200

    added showModal() function (version 1)
    The desired result is not achieved. No modal is displayed. Console logs error.

--- js/scripts.js
+++ js/scripts.js
@@ -5,41 +5,6 @@ var pokemonRepository = (function() {
   var repository = [];
   var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-/*  var bulbasaur = {
-    name : 'Bulbasaur',
-    height : 0.7,
-    type : ['grass', 'poison']
-  };
-  var squirtle = {
-    name : 'Squirtle',
-    height : 0.5,
-    type : ['water']
-  };
-  var charmander = {
-    name : 'Charmander',
-    height : 0.6,
-    type : ['fire']
-  };
-  var butterfree = {
-    name : 'Butterfree',
-    height : 1.1,
-    type : ['bug', 'flying']
-  };
-  var dragonite = {
-    name : 'Dragonite',
-    height : 2.2,
-    type : ['dragon', 'flying']
-  };
-  var krookodile = {
-    name : 'Krookodile',
-    height : 1.5,
-    type : ['dark', 'ground']
-  };
-*/
-
-  //pushing the Pokemon objects into the repository array
-  //repository.push(bulbasaur, squirtle, charmander, butterfree, dragonite, krookodile);
-
 
 
   //add-list-item function
@@ -72,7 +37,9 @@ var pokemonRepository = (function() {
   //showDetails funtion added
   function showDetails(pokemonItem) {
     pokemonRepository.loadDetails(pokemonItem).then(function() {
-    console.log(pokemonItem); });
+      //displays details in a modal
+      pokemonRepository.showModal(pokemonItem);
+    });
   }
 
   //add function to add objects into repository
@@ -115,24 +82,95 @@ var pokemonRepository = (function() {
       item.imageUrl = details.sprites.front_default;
       item.height = details.height;
       item.types = Object.keys(details.types);
+      item.weight = details.weight;
     }).catch(function (e) {
       console.error(e);
     });
   }
 
+//creating modal function
+function showModal(pokemonItem) {
+  var $modalContainer = document.querySelector('#modal-container');
+
+  //clearing all existing modal content
+  $modalContainer.innerHTML = '';
+
+  //creating element in DOM
+  var modal = document.createElement('div');
+  //adding class to div element
+  modal.classList.add('modal');
+
+  //creating the close button in modal
+  var closeButtonElement = document.createElement('button');
+  closeButtonElement.classList.add('modal-close');
+  closeButtonElement.innerText = 'Close';
+
+  //adding eventListener for close button
+  closeButtonElement.addEventListener('click', hideModal);
+
+  //creating pokemon elements to display in modal
+  var nameElement = document.createElement('h1');
+  nameElement.innerText = pokemonItem.name;
+
+  var imageElement = document.createElement('img');
+  imageElement.classList.add('modal-img');
+  imageElement.setAttribute("src", item.imageUrl);
+
+  var heightElement = document.createElement('p');
+  heightElement.innerText = 'height : ' + item.height;
+
+  var weightElement = document.createElement('p');
+  weightElement.innterText = 'weight : ' + item.weight;
+
+  //appending each element to modal
+  modal.appendChild(closeButtonElement);
+  modal.appendChild(nameElement);
+  modal.appendChild(imageElement);
+  modal.appendChild(heightElement);
+  modal.appendChild(weightElement);
+  $modalContainer.appendChild(modal);
+
+  //add class to make modal visible
+  $modalContainer.classList.add('is-Visible');
+}
+
+//hides modal when 'close' button is clicked
+function hideModal() {
+  var $modalContainer = document.querySelector('#modal-container');
+  $modalContainer.classList.remove('is-visible');
+}
+
+//hide modal when ECS on keyboard is pressed down
+window.addEventListener('keydown', (e) => {
+  var $modalContainer = document.querySelector('#modal-container');
+  if (e.key === 'Escape' && $modalContainer.classList.contains('is-visible')){
+    hideModal();
+  }
+})
+
+//hide modal if clicked outside of it
+var $modalContainer = document.querySelector('#modal-container');
+$modalContainer.addEventListener('click', (e) => {
+  var target = e.target;
+  if(target === $modalContainer){
+    hideModal();
+  }
+});
 
   //return function:
  return {
    add: add,
    getAll: getAll,
    addListItem: addListItem,
-   showDetails: showDetails,
+   //showDetails: showDetails,
    loadList: loadList,
-   loadDetails: loadDetails
+   loadDetails: loadDetails,
+   showModal: showModal,
+   hideModal: hideModal
   };
 })(); // IIFE wrapping closed
 
-//calling addListItem for each Pokemon
+//loading the data from API
 pokemonRepository.loadList().then(function() {
   // Now the data is loaded!
   pokemonRepository.getAll().forEach(function(pokemonItem){
